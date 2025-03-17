031f34ce0cf026ceaa65fc4389dbd37a916719e0
Jek John Tan <jetan@u91z05s03.42kl.edu.my>
2025-03-17 17:36:04 +0800

    add file writing function using ofstream

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:05:56 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/17 16:10:43 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/17 17:34:58 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@
 
 int main()
 {
-	std::ifstream input_file("test.txt");
+	std::ifstream infile("infile.txt");
+	std::ofstream outfile("outfile.txt");
 	std::string str;
 	
-	while (getline(input_file, str))
+	while (getline(infile, str))
 	{
-		std::cout << str;
+		outfile << str << std::endl;
 	}
 }
