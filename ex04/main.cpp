b010b0580c00d0aa6ee5ec4e035d8fb81b5a1350
Jek John Tan <jetan@u91z05s03.42kl.edu.my>
2025-03-17 16:12:59 +0800

    add file readin function using ifstream

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,13 +6,21 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:05:56 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/17 15:34:43 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/17 16:10:43 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
+#include <iostream>
+#include <string>
 
 int main()
 {
+	std::ifstream input_file("test.txt");
+	std::string str;
 	
+	while (getline(input_file, str))
+	{
+		std::cout << str;
+	}
 }
