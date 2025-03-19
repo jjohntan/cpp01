b7f07d5bb1709548bc6412b437522e60d3c82031
Jek John Tan <jetan@u91z01s01.42kl.edu.my>
2025-03-19 17:07:55 +0800

    handle file reading failed

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:05:56 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/19 15:51:20 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/19 17:05:57 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,15 @@ int main(int ac, char **av)
 {
 	(void)ac;
 	std::ifstream infile(av[1]);
+	if (!infile)
+	{
+		std::cerr << "Error failed to open file" << std::endl;
+		return (1);
+	}
 	std::string newfile = std::string(av[1]) + ".replace";
 	std::ofstream outfile(newfile.c_str());
+	std::string s1;
+	std::string s2;
 	std::string line;
 	
 	while (getline(infile, line))
