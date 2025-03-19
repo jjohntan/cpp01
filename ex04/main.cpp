e6cbc85c615266dc8f66046fa52933390c90b474
Jek John Tan <jetan@u91z01s01.42kl.edu.my>
2025-03-19 18:49:13 +0800

    add substring before s1

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:05:56 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/19 17:32:18 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/19 18:38:55 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,16 @@ int main(int ac, char **av)
 	std::string s1 = av[2];
 	std::string s2 = av[3];
 	std::string line;
+	size_t pos;
+	std::string before;
 	while (getline(infile, line))
 	{
+		pos = line.find(s1);
+		if (pos != std::string::npos)
+		{
+			before = line.substr(0, pos);
+			std::cout << before << std::endl;
+		}
 		outfile << line << std::endl;
 	}
 	infile.close();
