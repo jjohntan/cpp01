8a4493f1503285c2cc24ecdc4ac6890469f5741d
Jek John Tan <jetan@u91z01s01.42kl.edu.my>
2025-03-19 16:34:53 +0800

    add <filename>.replace and add c_str function

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:05:56 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/19 15:18:49 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/19 15:51:20 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int main(int ac, char **av)
 {
 	(void)ac;
 	std::ifstream infile(av[1]);
-	std::ofstream outfile("test.txt.replace");
+	std::string newfile = std::string(av[1]) + ".replace";
+	std::ofstream outfile(newfile.c_str());
 	std::string line;
 	
 	while (getline(infile, line))
