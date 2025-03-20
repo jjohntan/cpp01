8c6aca61af63591cc6ac89d3b21534cc29f06bdb
Jek John Tan <jetan@u91z04s02.42kl.edu.my>
2025-03-20 20:08:28 +0800

    implement info function

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 16:52:25 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/20 19:26:41 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/20 20:03:32 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,15 @@ void Harl::debug()
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
+void Harl::info()
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
 void Harl::complain( std::string level )
 {
 	if (level == "DEBUG")
 		this->debug();
+	else if (level == "INFO")
+		this->info();
 }
