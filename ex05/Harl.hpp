fdd8fa053f6c220184f504a164932a6d546b43f6
Jek John Tan <jetan@u91z04s02.42kl.edu.my>
2025-03-20 16:59:10 +0800

    add Harl class

---
+++ ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 16:53:57 by jetan             #+#    #+#             */
+/*   Updated: 2025/03/20 16:56:45 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <string>
+
+class Harl
+{
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+	public:
+		Harl();
+		~Harl();
+		void complain( std::string level );
+};
+
+#endif
