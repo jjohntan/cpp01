9da25aaf1fb11e8b1634e6449ff3503653a5c675
Jek John Tan <jetan@u91z04s02.42kl.edu.my>
2025-03-20 20:15:41 +0800

    implement warning function

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 16:52:25 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/20 20:03:32 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/20 20:15:01 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,19 @@ Harl::~Harl()
 {
 }
 
-void Harl::debug()
+void Harl::debug( void )
 {
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
-void Harl::info()
+void Harl::info( void )
 {
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning( void )
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::complain( std::string level )
@@ -37,4 +42,6 @@ void Harl::complain( std::string level )
 		this->debug();
 	else if (level == "INFO")
 		this->info();
+	else if (level == "WARNING")
+		this->warning();
 }
