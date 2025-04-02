2e2ce6bee87f690fa92a1e7a08ed0c9625f0e600
Jek John Tan <jetan@u82z01s01.42kl.edu.my>
2025-04-02 16:31:16 +0800

    remove library

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,14 +6,13 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:05:56 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/02 16:17:46 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/02 16:30:51 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <cstdlib>
 
 int main(int ac, char **av)
 {
